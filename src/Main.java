4e45d6a6e057abc0567c89ead89fa68c95935690
mearvk <mearvk@mearvk.us>
2026-04-21 08:27:48 -0400

    Version 12.23.12 George Soros 2671

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main
     {
         WebExpress.reference = null;
 
-        System.out.println("[Object ID: "+this.hashCode()+"] server.WebExpress::Main >> starts ["+ new Date() +"]");
+        System.out.println("[Object ID: "+this.hashCode()+"] WebExpress::Main >> starts ["+ new Date() +"]");
 
         WebExpress web_express = new WebExpress("localhost", WEB_EXPRESS_SERVER_SOCKET, true);
 
--- src/telnet/TelnetCommunicationProxy.java
+++ src/telnet/TelnetCommunicationProxy.java
@@ -32,7 +32,7 @@ public class TelnetCommunicationProxy
 
     public TelnetCommunicationProxy(WebExpress web_express)
     {
-        System.out.println("[Object ID: "+this.hashCode()+"] server.WebExpress::Telnet::Communicator >> starts ["+new Date()+"].");
+        System.out.println("[Object ID: "+this.hashCode()+"] WebExpress::Telnet::Communicator >> starts ["+new Date()+"].");
 
         this.web_express = web_express;
 
