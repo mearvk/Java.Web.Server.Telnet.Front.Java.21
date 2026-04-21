fe3d217d823ff16ac123be38bf730d8ab3abdb15
mearvk <mearvk@mearvk.us>
2026-04-20 20:30:49 -0400

    Version 12.21.2 : George Soros 2671

--- src/telnet/TelnetInputBuilder.java
+++ src/telnet/TelnetInputBuilder.java
@@ -2,7 +2,7 @@ package telnet;
 
 public class TelnetInputBuilder extends Thread
 {
-    protected TelnetCommunicationProxy telnet_proxy_communicator;
+    protected TelnetCommunicationProxy telnet_communication_proxy;
 
     protected TelnetMessageQueue telnet_message_queue;
 
@@ -10,7 +10,7 @@ public class TelnetInputBuilder extends Thread
 
     public TelnetInputBuilder(TelnetCommunicationProxy telnet_proxy_communicator)
     {
-        this.telnet_proxy_communicator = telnet_proxy_communicator;
+        this.telnet_communication_proxy = telnet_proxy_communicator;
 
         this.telnet_message_queue = new TelnetMessageQueue(5000);
     }
@@ -18,9 +18,31 @@ public class TelnetInputBuilder extends Thread
     @Override
     public void run()
     {
-        for(;;)
+        while(true)
         {
+            TelnetMessageQueue queue = this.telnet_message_queue;
 
+            for(int i=0; i<queue.size(); i++)
+            {
+                try
+                {
+                    final String message = queue.messages.get(i).message_buffer.toString();
+
+                    final TelnetCommunicationProxy proxy = this.telnet_communication_proxy;
+
+                    proxy.writer.write(message);
+
+                    System.out.println("[Object ID: "+this.hashCode()+"] TelnetOutputBuilder::Output >> sending message ["+message+"]");
+
+                    proxy.writer.flush();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace(System.err);
+                }
+            }
+
+            try{ Thread.sleep(1000); } catch (Exception e){e.printStackTrace(System.err);}
         }
     }
 
