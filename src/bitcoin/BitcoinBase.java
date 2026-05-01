939019859fe4f5789cf641b6efaddd4706e11a3c
mearvk <mearvk@mearvk.us>
2026-04-30 23:16:11 -0400

    Version 33.12.13 George Soros 2671 [now w/ Legal & Governmental Committment] [AES 2 Edition] [Bitcoin Edition] [Durham NC Edition]

--- src/bitcoin/BitcoinBase.java
+++ src/bitcoin/BitcoinBase.java
@@ -62,57 +62,7 @@ public class BitcoinBase
         CommonRails.printSystemComponent(this.hashCode(),"WebExpress::Bitcoin >> opens in Japan on Date [["+JAPANDate.PACIFIC_Time +"]]");
     }
 
-    public static class BitcoinMessageOrderer extends Thread
-    {
-        public ArrayList<BitcoinMessage> bitcoin_messages = new ArrayList<BitcoinMessage>(5000);
 
-        public BitcoinBase bitcoin;
-
-        public BitcoinMessageOrderer(BitcoinBase bitcoin)
-        {
-            this.bitcoin = bitcoin;
-        }
-
-        @Override
-        public void run()
-        {
-            while(true)
-            {
-
-            }
-        }
-
-        public synchronized void add(BitcoinMessage bitcoin_message)
-        {
-            this.bitcoin_messages.add(bitcoin_message);
-        }
-
-        public synchronized void remove(BitcoinMessage bitcoin_message)
-        {
-            this.bitcoin_messages.remove(bitcoin_message);
-        }
-
-        public synchronized void clear(BitcoinMessage bitcoin_message)
-        {
-            this.bitcoin_messages.clear();
-        }
-
-        public static class BitcoinMessage
-        {
-            protected Date date;
-
-            protected Socket socket;
-
-            protected InetAddress inet_address;
-
-            protected StringBuffer message_buffer;
-
-            public BitcoinMessage()
-            {
-
-            }
-        }
-    }
 
     public void send_message(StringBuffer buffer)
     {
---
+++ src/bitcoin/BitcoinMessageOrderer.java
@@ -0,0 +1,58 @@
+package bitcoin;
+
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class BitcoinMessageOrderer extends Thread
+{
+    public ArrayList<BitcoinMessage> bitcoin_messages = new ArrayList<BitcoinMessage>(5000);
+
+    public BitcoinBase bitcoin;
+
+    public BitcoinMessageOrderer(BitcoinBase bitcoin)
+    {
+        this.bitcoin = bitcoin;
+    }
+
+    @Override
+    public void run()
+    {
+        while(true)
+        {
+
+        }
+    }
+
+    public synchronized void add(BitcoinMessage bitcoin_message)
+    {
+        this.bitcoin_messages.add(bitcoin_message);
+    }
+
+    public synchronized void remove(BitcoinMessage bitcoin_message)
+    {
+        this.bitcoin_messages.remove(bitcoin_message);
+    }
+
+    public synchronized void clear(BitcoinMessage bitcoin_message)
+    {
+        this.bitcoin_messages.clear();
+    }
+
+    public static class BitcoinMessage
+    {
+        protected Date date;
+
+        protected Socket socket;
+
+        protected InetAddress inet_address;
+
+        protected StringBuffer message_buffer;
+
+        public BitcoinMessage()
+        {
+
+        }
+    }
+}
