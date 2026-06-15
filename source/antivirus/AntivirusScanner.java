6cdc1ad296f85cdd5b892468be294945eae1e1f4
mearvk <mearvk@outlook.com>
2026-06-15 05:02:47 -0400

    System Touch

--- source/antivirus/AntivirusScanner.java
+++ source/antivirus/AntivirusScanner.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.util.HexFormat;
 import java.util.Map;
