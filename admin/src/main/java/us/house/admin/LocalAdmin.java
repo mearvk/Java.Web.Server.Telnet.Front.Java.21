99d139486845f0ccc5a49ad5aca9c4ba071f0ff6
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-16 15:38:57 -0400

    Add LocalAdmin descriptor dates

--- admin/src/main/java/us/house/admin/LocalAdmin.java
+++ admin/src/main/java/us/house/admin/LocalAdmin.java
@@ -1,3 +1,8 @@
+/**
+ * JWSTF Local Administration console.
+ * Descriptor introduced: 2026-08-24.
+ * Descriptor updated: 2026-09-16.
+ */
 package us.house.admin;
 
 import javafx.application.Application;
